39c010c8dc6e722dc9aef242de30e05403b07a23
Roman <romanshliakhov@gmail.com>
2023-09-21 14:05:39 +0300

    add animation

--- source/js/_components.js
+++ source/js/_components.js
@@ -2,6 +2,7 @@ import './components/modals.js';
 import './components/counter.js';
 import './components/replaceEl.js';
 import './components/burger.js';
+import './components/animation.js';
 
 // import './components/accordions.js';
 // import './components/sliders.js';
@@ -10,7 +11,7 @@ import './components/burger.js';
 // import './components/spollers.js';
 // import './components/tabs.js';
 // import './components/parallax.js';
-// import './components/animation.js';
+
 // import './components/multiplays.js';
 // import './components/phoneMask.js';
 // import './components/search.js';
